8cd5a0f4e309e6418d39e26e9ea44837f3caa65e
Patrick Candlin <pctennislessons@gmail.com>
2019-04-20 21:15:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var a,b 
 function add(a,b) {
- return a + b  
+ return a + b;  
 }
 
 function subtract(a,b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a,b) {
-  return a * b 
+  return a * b;
 }
 
 function divide(a,b) {
   return a / b
 }
 function inc(a) {
-  return a++
+  return a++;
 }
-functionn dec(a) {
-  return a--
+function dec(a) {
+  return a--;
 }
